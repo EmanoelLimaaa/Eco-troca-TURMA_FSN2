881c656bec90bac41b3a5dbeaa504557c6eef9c1
sibele-lino <sibelecaetano7@gmail.com>
2025-08-09 18:33:55 -0300

    configura estrutural inicial daAPI e testa rota de usuarios

--- Back-end/src/routes/usuarioRoutes.js
+++ Back-end/src/routes/usuarioRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { listarUsuarios, criarUsuario } from '../controllers/usuarioController.js';
+import { listarUsuarios, criarUsuario, teste } from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
 router.get('/', listarUsuarios);
 router.post('/', criarUsuario);
+
+router.get('/teste', teste);
 
 export default router;
