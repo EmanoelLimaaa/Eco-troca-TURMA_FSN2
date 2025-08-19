3d0d91a737c697f01c62584c084810b8b29fd915
sibele-lino <sibelecaetano7@gmail.com>
2025-08-18 21:08:24 -0300

    implementa rotas de categorias e propostas com controllers

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import categoriaRoutes from './routes/categoriaRoutes.js';
+import propostaRoutes from './routes/propostaRoutes.js';
 
 const app = express();
 
@@ -8,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/usuarios', usuarioRoutes);
+app.use('/categorias', categoriaRoutes);
+app.use('/propostas', propostaRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
-
